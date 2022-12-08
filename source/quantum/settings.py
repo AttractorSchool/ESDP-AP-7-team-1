176de306c640e4a556c64accb04f9ac2a7ae6d1e
Nadezhda Khovrich <hope28281@gmail.com>
2022-12-09 01:19:42 +0600

    Добавлена переменная окружения DB_NAME

--- source/quantum/settings.py
+++ source/quantum/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'quantum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'quantum',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
