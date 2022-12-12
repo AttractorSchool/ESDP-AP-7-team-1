ef377016d9c5eb33f193d19423ec19fcb1eeb397
Nadezhda Khovrich <hope28281@gmail.com>
2022-12-12 22:15:48 +0600

    [QUAN-34]: Создан GroupingsScheduleView для просмотра расписания групп

--- source/education/urls.py
+++ source/education/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from education.views.base import IndexView
-from education.views.schedule import ScheduleView
+from education.views.schedule import StudentScheduleView, GroupingsScheduleView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='main'),
-    path('schedule/', ScheduleView.as_view(), name='schedule'),
+    path('schedule/', StudentScheduleView.as_view(), name='schedule'),
+    path('schedule-groupings/', GroupingsScheduleView.as_view(), name='schedule_groupings'),
+
 ]
--- source/education/views/schedule.py
+++ source/education/views/schedule.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
-from education.models import Schedule
+from education.models import Schedule, Grouping
 
 
-class ScheduleView(ListView):
+class StudentScheduleView(ListView):
+    '''для вывода расписания залогиневшегося студента'''
     template_name = 'education/student_schedule.html'
     model = Schedule
 
@@ -20,3 +21,32 @@ class ScheduleView(ListView):
             for schedule in grouping.schedules.all():
                 context[schedule.week_day].append((grouping, schedule.time_start))
         return context
+
+
+class GroupingsScheduleView(ListView):
+    template_name = 'education/groupings_schedule.html'
+    model = Schedule
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        groupings = Grouping.objects.all()
+        list_groupings = []
+        for grouping in groupings:
+            week_days = [grouping.name, '', '', '', '', '', '']
+            for schedule in grouping.schedules.all():
+                if schedule.week_day == 'monday':
+                    week_days[1] = schedule.time_start
+                elif schedule.week_day == 'tuesday':
+                    week_days[2] = schedule.time_start
+                elif schedule.week_day == 'wednesday':
+                    week_days[3] = schedule.time_start
+                elif schedule.week_day == 'thursday':
+                    week_days[4] = schedule.time_start
+                elif schedule.week_day == 'friday':
+                    week_days[5] = schedule.time_start
+                elif schedule.week_day == 'saturday':
+                    week_days[6] = schedule.time_start
+            list_groupings.append(week_days)
+        context['list_groupings'] = list_groupings
+        return context
+
