e58b7cb728d98214ca4181af7e4251e928f8e68e
Isa Bakhtov <issab79@icloud.com>
2022-12-12 16:19:01 +0600

    [QUAN-13] Добавлен функционал изменения профиля пользователя

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -5,6 +5,7 @@ from accounts.views.logout_view import logout_view
 from accounts.views.profile_view import ProfileView
 from accounts.views.register import RegisterView
 from accounts.views.delete_view import UserDeleteView
+from accounts.views.change_view import AccountChangeView
 
 urlpatterns = [
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
@@ -12,5 +13,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path("account/<int:pk>/delete/", UserDeleteView.as_view(), name= "user_delete"),
-    path("account/<int:pk>/confirm_delete/", UserDeleteView.as_view(), name= "confirm_user_delete")
+    path("account/<int:pk>/confirm_delete/", UserDeleteView.as_view(), name= "confirm_user_delete"),
+    path('account/<int:pk>/change', AccountChangeView.as_view(), name='account_change')
 ]
