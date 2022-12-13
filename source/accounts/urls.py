b21bdf36b7515a6ce22de9c2aa278ef8922a2541
Almukhan Chingiz <chingizpain@gmail.com>
2022-12-13 08:47:13 +0600

    [QUAN-38]: Вывести заявки в соответствующем разделе --Созданы вью для отображения и изменения статуса заявок --Создана форма для изменения статуса --Создана вью для отображения заявок

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -4,9 +4,6 @@ from accounts.views.admin_register_view import AdminRegisterView
 from accounts.views.login_view import LoginView
 from accounts.views.logout_view import logout_view
 from accounts.views.profile_view import ProfileView
-from accounts.views.student_register_view import StudentRegisterView
-from accounts.views.teacher_register_view import TeacherRegisterView
-# from accounts.views.register import RegisterView
 from accounts.views.delete_view import UserDeleteView
 from accounts.views.change_view import AccountChangeView
 from accounts.views.student_register_view import StudentRegisterView
@@ -20,7 +17,7 @@ urlpatterns = [
     path('register_admin/', AdminRegisterView.as_view(), name='register_admin'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
-    path("account/<int:pk>/delete/", UserDeleteView.as_view(), name= "user_delete"),
-    path("account/<int:pk>/confirm_delete/", UserDeleteView.as_view(), name= "confirm_user_delete"),
+    path("account/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
+    path("account/<int:pk>/confirm_delete/", UserDeleteView.as_view(), name="confirm_user_delete"),
     path('account/<int:pk>/change', AccountChangeView.as_view(), name='account_change')
 ]
--- source/education/urls.py
+++ source/education/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from education.views.base import IndexView
+from education.views.change_application_status_view import ChangeStatusView
 from education.views.schedule import StudentScheduleView, GroupingsScheduleView
+from education.views.show_applications_view import ShowApplicationsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='main'),
     path('schedule/', StudentScheduleView.as_view(), name='schedule_student'),
     path('schedule-groupings/', GroupingsScheduleView.as_view(), name='schedule_groupings'),
+    path('applications/', ShowApplicationsView.as_view(), name='show_applications'),
+    path('change/status/<int:pk>', ChangeStatusView.as_view(), name='change_status')
 
 ]
