697260c4b9c6261c4793e5aaca60b18cb3379ee9
Yugai Denis <yugai_denis@mail.ru>
2022-12-16 20:53:16 +0600

    [QUAN-60]: Содание модели Package

---
+++ source/education/models/packages.py
@@ -0,0 +1,14 @@
+﻿from django.db import models
+
+
+class Package(models.Model):
+    title = models.CharField(verbose_name='Название', max_length=30, unique=True),
+    qty = models.PositiveIntegerField(verbose_name='Количество', blank=True, null=True)
+    sum = models.DecimalField(verbose_name='Сумма', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пакет'
+        verbose_name_plural = 'Пакеты'
