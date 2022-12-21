2c58c6e1fd95d86d06af97195fb081a7fa05aa49
Almukhan Chingiz <chingizpain@gmail.com>
2022-12-21 17:35:46 +0600

    [QUAN-78] Откорректировал модель заявок

--- source/education/models/application.py
+++ source/education/models/application.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.db.models import TextChoices
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Status(models.Model):
@@ -20,26 +24,31 @@ class StudentSex(TextChoices):
     FEMALE = 'FEMALE', 'Qyz bala'
 
 
+def validate_length(value):
+    an_integer = value
+    a_string = str(an_integer)
+    if len(a_string) > 12:
+        raise ValidationError(_('%(value) инн более 12 символов'))
+
+
 class Application(models.Model):
     """Созданные заявки пользователями"""
     applicant_name = models.CharField(verbose_name='Имя ученика', max_length=30)
     applicant_surname = models.CharField(verbose_name='Фамилия ученика', max_length=30)
     email = models.EmailField(verbose_name='Электронная почта ученика', blank=True)
-    phone = models.CharField(verbose_name='Номер телефона ученика', max_length=18)
-    school = models.IntegerField(verbose_name='Номер школы', null=True, blank=True,
-                                 help_text="Вводить только цифры")
+    phone = PhoneNumberField(unique=False, null=True, blank=True, verbose_name='Номер ученика')
+    school = models.IntegerField(verbose_name='Номер школы', null=True, blank=True, help_text="Вводить только цифры")
     class_number = models.CharField(verbose_name='Номер класса', max_length=3, null=True, blank=True)
     shift = models.IntegerField(verbose_name='Номер смены', null=True, blank=True,
                                 help_text="Вводить только цифры")
     birth_date = models.DateField(verbose_name='Дата рождения ученика', max_length=10, null=True, blank=True,
                                   help_text="Заполнить в формате дд.мм.гггг")
     sex = models.CharField(verbose_name='Пол ученика', choices=StudentSex.choices, max_length=100, default='Undefined')
-
     parents_surname = models.CharField(verbose_name='Фамилия родителя', null=True, blank=True, max_length=30)
     parents_name = models.CharField(verbose_name='Имя родителя', max_length=30, null=True, blank=True)
-    parents_inn = models.IntegerField(verbose_name='ИНН родителя', null=True, blank=True,
-                                      help_text="Вводить только цифры")
-    parents_phone = models.CharField(verbose_name='Номер телефона родителя', max_length=18, null=True, blank=True)
+    parents_inn = models.PositiveBigIntegerField(verbose_name='ИНН родителя', null=True, blank=True,
+                                                 help_text="Вводить только цифры", validators=[validate_length])
+    parents_phone = PhoneNumberField(unique=False, null=True, blank=True, verbose_name='Номер родителя')
     parents_email = models.EmailField(verbose_name='Электронная почта родителя', null=True, blank=True)
     address = models.CharField(verbose_name='Адрес проживания', max_length=13, null=True, blank=True,
                                help_text="Вводить через запятую: населенный пункт, улица, номер дома, номер квартиры")
@@ -55,7 +64,7 @@ class Application(models.Model):
         verbose_name='Статус заявки',
         through='education.ApplicationStatus',
         related_name='applications',
-        )
+    )
     is_deleted = models.BooleanField(default=False)
     discount = models.ForeignKey(
         verbose_name="Льгота",
@@ -64,7 +73,7 @@ class Application(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        )
+    )
 
     def __str__(self):
         return f'Заявка от: {self.applicant_name}'
