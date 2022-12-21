bff8d0e747a03264cf08441ef918e26242926279
Almukhan Chingiz <chingizpain@gmail.com>
2022-12-21 19:14:47 +0600

    [QUAN-77] Откорректировал форму редактирования заявок --Добавил выбор смены через select --Добавил выбор желательного времени обучения --Добавил выбор даты рождения через календарь --Добавил в модель заявки ограничение по минимальному числу

--- source/education/forms/application_edit_form.py
+++ source/education/forms/application_edit_form.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.utils.datastructures import MultiValueDict
+from django.core.validators import MinValueValidator
+
+from phonenumber_field.formfields import PhoneNumberField
 
 from education.models import Application, StudentSex, Subject
 
@@ -11,15 +14,23 @@ class M2MSelect(forms.Select):
         return data.get(name, None)
 
 
+SHIFTS = ((1, 1), (2, 2), (3, 3))
+LESSONS_TIME = (('13:30', '13:30'), ('15:00', '15:00'), ('16:30', '16:30'))
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class ApplicationEditForm(forms.ModelForm):
     subjects = forms.ModelMultipleChoiceField(label='Предметы к обучению', queryset=Subject.objects.all(),
-        widget=forms.CheckboxSelectMultiple(
-        attrs={
-            'class': 'subject-check',
-        }))
+                                              widget=forms.CheckboxSelectMultiple(
+                                                  attrs={
+                                                      'class': 'subject-check',
+                                                  }))
 
     sex = forms.ChoiceField(label='Пол', choices=StudentSex.choices)
-    phone = forms.CharField(label='Телефон ученика', widget=forms.TextInput(
+    phone = PhoneNumberField(region='KZ', label='Телефон', widget=forms.TextInput(
         attrs={
             'class': 'phone-mask',
             'placeholder': 'Телефон',
@@ -33,6 +44,10 @@ class ApplicationEditForm(forms.ModelForm):
         attrs={
             'class': 'mx-2',
         }))
+    shift = forms.ChoiceField(label='Смена', choices=SHIFTS, required=False, widget=forms.Select)
+    lesson_time = forms.ChoiceField(label='Желательное время', choices=LESSONS_TIME, required=False,
+                                    widget=forms.Select)
+    birth_date = forms.DateField(widget=DateInput, required=False, label='День рождения')
 
     class Meta:
         model = Application
--- source/education/models/application.py
+++ source/education/models/application.py
@@ -1,9 +1,8 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.db.models import TextChoices
-
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Status(models.Model):
@@ -37,7 +36,8 @@ class Application(models.Model):
     applicant_surname = models.CharField(verbose_name='Фамилия ученика', max_length=30)
     email = models.EmailField(verbose_name='Электронная почта ученика', blank=True)
     phone = models.CharField(verbose_name='Номер телефона ученика', max_length=18)
-    school = models.IntegerField(verbose_name='Номер школы', null=True, blank=True, help_text="Вводить только цифры")
+    school = models.IntegerField(verbose_name='Номер школы', null=True, blank=True, validators=[MinValueValidator(0)],
+                                 help_text="Вводить только цифры")
     class_number = models.CharField(verbose_name='Номер класса', max_length=3, null=True, blank=True)
     shift = models.IntegerField(verbose_name='Номер смены', null=True, blank=True,
                                 help_text="Вводить только цифры")
@@ -47,14 +47,15 @@ class Application(models.Model):
     parents_surname = models.CharField(verbose_name='Фамилия родителя', null=True, blank=True, max_length=30)
     parents_name = models.CharField(verbose_name='Имя родителя', max_length=30, null=True, blank=True)
     parents_inn = models.PositiveBigIntegerField(verbose_name='ИНН родителя', null=True, blank=True,
-                                                 help_text="Вводить только цифры", validators=[validate_length])
+                                                 help_text="Вводить только цифры",
+                                                 validators=[validate_length, MinValueValidator(0)])
     parents_phone = models.CharField(verbose_name='Номер телефона родителя', max_length=18, null=True, blank=True)
     parents_email = models.EmailField(verbose_name='Электронная почта родителя', null=True, blank=True)
     address = models.CharField(verbose_name='Адрес проживания', max_length=200, null=True, blank=True,
                                help_text="Вводить через запятую: населенный пункт, улица, номер дома, номер квартиры")
     lesson_time = models.CharField(verbose_name="Желательное время обучения", null=True, blank=True, max_length=250)
     subjects = models.ManyToManyField(to='education.Subject', related_name='applications', blank=True)
-    sum = models.IntegerField(verbose_name='Cумма пакета', null=True, blank=True)
+    sum = models.IntegerField(verbose_name='Cумма пакета', null=True, blank=True, validators=[MinValueValidator(0)])
     contract = models.FileField(verbose_name="Подписанный договор", null=True, blank=True,
                                 help_text="Загружать pdf подписанного договора", upload_to='contracts/')
     payed = models.BooleanField(verbose_name="Оплачено", default=False)
