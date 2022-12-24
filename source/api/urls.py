46ec529dd1e44ae69687a51fbb5ed7b861948518
Almukhan Chingiz <chingizpain@gmail.com>
2022-12-24 19:47:19 +0600

    [QUAN-86] Добавил возможность добавления студента через заявку --Обновил модель заявки, а именно поле с номером телефона --В форме редактирования заявки и ее создания также обновил поле с телефоном --Обновил скрипт по подсчету суммы пакета --Добавил скрипт с аякс запросом на создание пользователя --Прописал пути в urls

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
+from api.views.create_user_by_application_view import CreateUser
 from api.views.sum_package_view import SumPackageView
 
 urlpatterns = [
     path('get_sum', csrf_exempt(SumPackageView.as_view()), name='sum_package'),
+    path('create-user-by-application/<int:pk>', CreateUser.as_view(), name='create_user_by_application'),
 ]
