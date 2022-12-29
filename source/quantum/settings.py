defd6a91d520457654fda0f4633a23e13387d09b
Nadezhda Khovrich <hope28281@gmail.com>
2022-12-29 22:55:01 +0600

    [FIX] исправление ошибки загрузки переменной окружения DEBUG

--- source/quantum/settings.py
+++ source/quantum/settings.py
@@ -15,7 +15,7 @@ load_dotenv(os.path.join(BASE_DIR, 'environment', '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ['*']
 
