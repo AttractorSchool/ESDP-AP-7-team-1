c6c0df9a586e8531899cd49b80eb65bf98457555
Almukhan Chingiz <chingizpain@gmail.com>
2023-01-09 23:35:27 +0600

    [QUAN-103] Добавить расчет скидки по льготам, откорректировать уведомление о оставленной заявке --Создал скрипт с ajax запросом для расчета скидки по льготе --Откорректировал уведомление о том, что заявка создана --Создал вью для расчета скидки по льготе --Прописал пути в urls

--- source/api/views/sum_package_view.py
+++ source/api/views/sum_package_view.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
 
-from education.models import Packet
+from education.models import Packet, Discount
 
 
 class SumPackageView(View):
@@ -18,3 +18,14 @@ class SumPackageView(View):
         return JsonResponse(data)
 
 
+class SumDiscountView(View):
+    def post(self, request, *args, **kwargs):
+        data = {}
+        if request.body:
+            ajax_data = json.loads(request.body)
+            get_package = get_object_or_404(Packet, qty=ajax_data.get('subject'))
+            get_discount = get_object_or_404(Discount, pk=ajax_data.get('discount'))
+            total = int(get_package.sum) - ((int(get_package.sum) * int(get_discount.discount_amount)) / 100)
+            data['total'] = total
+            data['discount'] = get_discount.discount_amount
+        return JsonResponse(data)
