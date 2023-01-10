57b2ecff5373444cc2ed3c0226d9a27309447784
Daniarbekov <daniarbekov1998@gmail.com>
2023-01-10 13:26:01 +0600

    [refactor] убрал лишний код

--- source/education/views/contracts.py
+++ source/education/views/contracts.py
@@ -26,9 +26,8 @@ MONTH = [
 ]
 
 
-def render_pdf(request, pk):
+def render_pdf(request, app):
     """Формирование PDF Файла"""
-    app = Application.objects.get(pk=pk)
     cur_date = datetime.datetime.now()
     month = MONTH[cur_date.month-1]
     html_string = render_to_string('education/contract_template.html',
@@ -59,7 +58,7 @@ def open_contract_pdf(request, *args, **kwargs):
 
 def send_contract(request, *args, **kwargs):
     app = Application.objects.get(pk=kwargs.get('pk'))
-    contract = render_pdf(request, app.pk)
+    contract = render_pdf(request, app)
     emails = [app.email, app.parents_email]
     for email in emails:
         email_msg = EmailMessage('Договор', body=contract, from_email='testeruly@yandex.kz', to=[email])
